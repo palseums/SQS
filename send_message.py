79baf4763d475f44b15afdf004fe6bef7dfa1848
palseums <57275214+palseums@users.noreply.github.com>
2024-03-20 22:22:33 -0500

    Initial commit

---
+++ send_message.py
@@ -0,0 +1,6 @@
+import boto3
+aws_mag_con = boto3.session.Session(profile_name="terraform",region_name="us-east-1")
+sqs_con = aws_mag_con.client('sqs')
+response = sqs_con.send_message(
+    QueueUrl='https://sqs.us-east-1.amazonaws.com/431995393781/test',
+    MessageBody='message6',)
